9473ac8b4c1fcb9c291aa3d3415f2a3a02e9281c
nairitprachanda <nairitprachanda@gmail.com>
2020-11-02 01:57:05 +0530

    [nairit] refactor: create stack and pushing data

---
+++ StacksAndQueues/StacksAndQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StacksandQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack !");
+            Console.WriteLine("\n");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+        }
+    }
+}
---
+++ StacksAndQueues/StacksAndQueues/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksandQueues
+{
+    public class Stack
+    {
+        private Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (top == null)
+                top = node;
+            else
+            {
+                node.next = top;
+                top = node;
+            }
+        }
+        public void Display()
+        {
+            if (top == null)
+            {
+                Console.WriteLine("Empty Stack !!");
+            }
+            else
+            {
+                Node temp = top;
+                while (temp != null)
+                {
+                    Console.Write(temp.data);
+                    if (temp.next != null)
+                        Console.Write("-> ");
+                    temp = temp.next;
+                }
+                Console.WriteLine("\n");
+            }
+        }
+    }
+}
